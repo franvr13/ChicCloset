78a09cdd669a8eccac75d78aceb4b248b98e0e22
CeliaFeria <90108736+CeliaFeria@users.noreply.github.com>
2024-07-18 11:07:10 +0200

    cambios

--- src/main/java/com/eoi/tiendaderopa/repositorios/RepoProducto.java
+++ src/main/java/com/eoi/tiendaderopa/repositorios/RepoProducto.java
@@ -4,6 +4,10 @@ import com.eoi.tiendaderopa.entidades.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.Set;
+
 @Repository
 public interface RepoProducto extends JpaRepository<Producto, Long> {
+
 }
