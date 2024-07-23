83ce9e56163e6d2672fbe08582e39662f639e34d
CeliaFeria <90108736+CeliaFeria@users.noreply.github.com>
2024-07-23 16:22:44 +0200

    pom con la dependencia de validation

---
+++ src/main/java/com/eoi/tiendaderopa/controladores/ChatController.java
@@ -0,0 +1,10 @@
+package com.eoi.tiendaderopa.controladores;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+
+
+}
---
+++ src/main/java/com/eoi/tiendaderopa/sockets/WebSocket.java
@@ -0,0 +1,28 @@
+package com.eoi.tiendaderopa.sockets;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocket implements WebSocketMessageBrokerConfigurer {
+    //portfolio is the HTTP URL for the endpoint to which a WebSocket (or SockJS) client needs to connect for the WebSocket handshake.
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websockethandshake");
+    }
+
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        //STOMP messages whose destination header begins with /app are routed to @MessageMapping methods in @Controller classes.
+        config.setApplicationDestinationPrefixes("/mensajes");
+        //Use the built-in message broker for subscriptions and broadcasting and route messages whose destination header begins with /topic or /queue to the broker.
+        config.enableSimpleBroker("/topic", "/queue");
+
+    }
+
+}
