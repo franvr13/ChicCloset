46d58fcf1646de49b5305f0b1c45f57a2d2cf0c6
CeliaFeria <90108736+CeliaFeria@users.noreply.github.com>
2024-08-17 14:05:41 +0200

    carrito funcional pero el pago es feo aun

--- src/main/java/com/eoi/tiendaderopa/controladores/PagoCtrl.java
+++ src/main/java/com/eoi/tiendaderopa/controladores/PagoCtrl.java
@@ -5,20 +5,64 @@ import com.eoi.tiendaderopa.servicios.SrvcPago;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
 @Controller
+@RequestMapping("/pago")
 public class PagoCtrl {
 
     @Autowired
     private SrvcPago pagoSrvc;
 
+
+
+    @GetMapping("/enviarinfo")
+    public String mostrarFormularioEnvio() {
+        return "enviarinfo";
+    }
+
+    @PostMapping("/datosPago")
+    public String mostrarFormularioPago(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("tel") String tel,
+            @RequestParam("country") String country,
+            @RequestParam("city") String city,
+            @RequestParam("address") String address,
+            Model model) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+        model.addAttribute("tel", tel);
+        model.addAttribute("country", country);
+        model.addAttribute("city", city);
+        model.addAttribute("address", address);
+
+        return "pago";
+    }
+
+    @PostMapping("/procesarPago")
+    public String procesarPago(
+            @RequestParam("cardNumber") String cardNumber,
+            @RequestParam("cardHolder") String cardHolder,
+            @RequestParam("expiryDate") String expiryDate,
+            @RequestParam("cvv") String cvv,
+            Model model) {
+
+        model.addAttribute("cardNumber", cardNumber);
+        model.addAttribute("cardHolder", cardHolder);
+        model.addAttribute("expiryDate", expiryDate);
+        model.addAttribute("cvv", cvv);
+
+        return "pagoexito";
+    }
+
+
+
     // Este parámetro sirve para mostrar una lista de pagos
-    @GetMapping("/pagos")
+    @GetMapping("/lista")
     public String listarPagos(Model model) {
 
         List<Pago> pago = pagoSrvc.buscarEntidades();
@@ -28,7 +72,7 @@ public class PagoCtrl {
     }
 
     // Este parámetro sirve para crear un nuevo pago
-    @PostMapping("/pagos")
+    @PostMapping("/nuevo")
     public String crearPago(@RequestBody Pago pago, Model model) {
         try {
             pagoSrvc.guardar(pago);
