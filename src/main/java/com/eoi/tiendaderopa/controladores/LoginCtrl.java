6abbe49adc8d4dd231c094e3451823639e768181
CeliaFeria <90108736+CeliaFeria@users.noreply.github.com>
2024-08-18 11:39:52 +0200

    logout funcional

--- src/main/java/com/eoi/tiendaderopa/controladores/LoginCtrl.java
+++ src/main/java/com/eoi/tiendaderopa/controladores/LoginCtrl.java
@@ -2,7 +2,11 @@ package com.eoi.tiendaderopa.controladores;
 
 import com.eoi.tiendaderopa.entidades.Usuario;
 import com.eoi.tiendaderopa.repositorios.RepoUsuario;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,6 +46,14 @@ public class LoginCtrl {
             model.addAttribute("msg", "Usuario no encontrado");
         }
         return "/productos";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 
 }
